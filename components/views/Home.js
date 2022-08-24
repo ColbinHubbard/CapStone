d8338dfa2887375a6a918d556e132e7efc72010a
Colbin Hubbard <colbinjustin@gmail.com>
2022-08-24 18:20:03 -0500

    capitolization

--- components/views/Home.js
+++ components/views/Home.js
@@ -6,11 +6,11 @@ export default () => html`
       <h1>
         Good <br />
         Music <br />
-        everywhere
+        Everywhere
       </h1>
     </div>
     <div class="right">
-      <p>Click Here to play</p>
+      <p>Click Here To Play</p>
       <img
         src="https://www.pngitem.com/pimgs/m/535-5357035_music-player-play-button-icon-music-player-png.png"
         id="icon"
